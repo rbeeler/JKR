f715003906f9cd9034a3a1842daa07d476c89e1e
leitto10 <46364179+leitto10@users.noreply.github.com>
2020-06-16 17:57:02 -0700

    adding more staff

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.static_folder = 'static'
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     return render_template("index.html")
 
 
-@app.route("/get")
+@app.route("/get", methods=['GET','POST'])
 def get_bot_response():
     userText = request.args.get('msg')
     return str(chat(userText))
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=8080)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
+    #app.run(debug=True)
